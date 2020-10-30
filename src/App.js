facf05444559820d0c1219abd44272d63eed4c44
Ivo <ivokalendar@icloud.com>
2020-10-30 23:03:04 +0100

    Completed the Search section...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
-import Test from './Test';
 
 const App = () => {
     useEffect(() => {
@@ -12,9 +13,12 @@ const App = () => {
     });
 
     return (
-        <div className='App'>
-            <Test />
-        </div>
+        <>
+            <SearchBar />
+            <div className='container'>
+                <Logs />
+            </div>
+        </>
     );
 };
 
