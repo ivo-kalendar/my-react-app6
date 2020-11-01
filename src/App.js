1bdddfd7ecea2549541a708889cb8f355f66c256
Ivo <ivokalendar@icloud.com>
2020-11-01 16:19:00 +0100

    Completed the UI...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -15,14 +18,17 @@ const App = () => {
     });
 
     return (
-        <>
+        <Fragment>
             <SearchBar />
             <div className='container'>
                 <AddBtn />
                 <AddLogModal />
+                <EditLogModal />
+                <AddTechModal />
+                <TechListModal />
                 <Logs />
             </div>
-        </>
+        </Fragment>
     );
 };
 
